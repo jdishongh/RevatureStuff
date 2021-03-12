f1a4c00e299a592b79ef86c436f9fc6f3df8b47d
Jason D <jdishongh@mymail.mines.edu>
2021-03-12 10:54:40 -0700

    Completed Exercises for Week1

---
+++ Week1/src/Exercise1.java
@@ -0,0 +1,9 @@
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		double value1 = 10.5;
+		double value2 = 11.5;
+		
+		System.out.println("Result: " + value1 * value2);
+	}
+}
---
+++ Week1/src/Exercise2.java
@@ -0,0 +1,13 @@
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		int x = 101;
+		
+		if(x == 3) {
+			System.out.println("x is equal to 3!");
+		}
+		else {
+			System.out.println("x is not equal to 3!");
+		}
+	}
+}
