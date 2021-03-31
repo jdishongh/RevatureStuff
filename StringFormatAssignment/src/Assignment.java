83447e28689c4dfee936df3ac3bd126fb70600c2
Jason D <jdishongh@mymail.mines.edu>
2021-03-30 18:34:11 -0600

    Introducing my family's 13 week old puppy Miles

---
+++ StringFormatAssignment/src/Assignment.java
@@ -0,0 +1,32 @@
+
+public class Assignment {
+	
+	public static void main(String[] args) {
+		System.out.printf("%s\n"," _____                               _____");
+		System.out.printf("%s\n","/     \\        _____________        /     \\");
+		System.out.printf("%s\n","|      \\      /             \\      /      |");
+		System.out.printf("%s\n","|       \\____/               \\____/       |");
+		System.out.printf("%s\n","\\      /                          \\      /");
+		System.out.printf("%s\n"," \\____/                            \\____/");
+		System.out.printf("%s\n","     /                              \\");
+		System.out.printf("%s","    /            ");
+		System.out.printf("%d        ", 0);
+		System.out.printf("%.1f", 0.0);
+		System.out.printf("    %s \n", "    \\");
+		System.out.printf("%s\n","   /                                  \\");
+		System.out.printf("%s\n","  /         ____________________       \\");
+		System.out.printf("%s\n","  |        /                    \\       |");
+		System.out.printf("%s\n","  |       /                      \\      |");
+		System.out.printf("%s\n","  |      /  (_                 _) \\     |");
+		System.out.printf("%s\n","  |      \\    \\               /   /     |");
+		System.out.printf("%s\n","  |       \\    \\_____________/   /      |");
+		System.out.printf("%s\n","  \\        \\                    /       /");
+		System.out.printf("%s\n","   \\        \\__________________/       /");
+		System.out.printf("%s\n","    \\                                 /");
+		System.out.printf("%s","     \\_____________");
+		System.out.printf("%b", true);
+		System.out.printf("%s\n","______________/");
+		
+
+	}
+}
